5b859aaeac789fb1b9d6ea34405f7eb368a8f94e
gooltoe <joerhoecommerce@gmail.com>
2022-10-19 16:33:20 -0700

    mint stuff

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount, usePrepareContractWrite, useContractWrite } from "wagmi";
+import {
+  useAccount,
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 import contractInterface from "../contract-abi.json";
 
 type Props = {};
@@ -16,11 +21,22 @@ function Hero({}: Props) {
     functionName: "mint",
   });
 
-  const { write: mint, isSuccess } = useContractWrite(config);
+  const {
+    data: mintData,
+    write: mint,
+    isLoading: isMintLoading,
+    isSuccess: isMintStarted,
+  } = useContractWrite(config);
+
+  const { isSuccess: txSuccess } = useWaitForTransaction({
+    hash: mintData?.hash,
+  });
+
+  const isMinted = txSuccess;
 
   return (
-    <div className="h-screen bg-blue-300 flex px-[10vw]">
-      <div className="flex items-center gap-4 justify-center">
+    <div className="md:h-screen bg-blue-300 flex px-[10vw] py-[10vh]">
+      <div className="flex flex-col md:flex-row items-center gap-4 justify-center">
         <div className="basis-1/2">
           <h1 className="font-bold text-3xl uppercase">blur bear</h1>
           <p className="text-lg font-semibold py-6">
@@ -51,8 +67,12 @@ function Hero({}: Props) {
                   mint?.();
                 }}
                 className="rounded-xl bg-white px-10 py-4 font-bold text-xl my-4 shadow-xl uppercase"
+                disabled={isMintLoading}
+                data-mint-loading={isMintLoading}
+                data-mint-started={isMintStarted}
               >
-                Mint
+                {isMintLoading && "Minting..."}
+                {!isMintLoading && "Mint"}
               </button>
             </div>
           ) : (
@@ -78,6 +98,11 @@ function Hero({}: Props) {
           )}
 
           <ConnectButton />
+          {isMinted && (
+            <div className="text-lg font-semibold py-6">
+              Transaction successful! Thanks for minting a Blur Bear!
+            </div>
+          )}
         </div>
         <div className="">
           <img src="/bear.png" alt="" />
