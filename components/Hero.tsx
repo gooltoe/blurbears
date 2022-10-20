719091bf929c827adfe747a87043041fa331867d
gooltoe <joerhoecommerce@gmail.com>
2022-10-19 17:23:14 -0700

    new colors!

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,7 @@ import {
   useWaitForTransaction,
 } from "wagmi";
 import contractInterface from "../contract-abi.json";
+import { CustomButton } from "./CustomButton";
 
 type Props = {};
 
@@ -35,14 +36,18 @@ function Hero({}: Props) {
   const isMinted = txSuccess;
 
   return (
-    <div className="md:h-screen bg-blue-300 flex px-[10vw] py-[10vh]">
+    <div className="md:h-screen bg-[#0a0504] flex flex-col px-[10vw] py-[10vh]">
+      <div className="p-6"></div>
       <div className="flex flex-col md:flex-row items-center gap-4 justify-center">
         <div className="basis-1/2">
-          <h1 className="font-bold text-3xl uppercase">blur bear</h1>
-          <p className="text-lg font-semibold py-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            placeat possimus tempore deserunt dolores? Impedit itaque, officiis
-            et ab sapiente repellendus obcaecati officia nemo vero?
+          <img src="/BLURBEARS.png" alt="" className="" />
+          <p className="text-lg font-semibold py-6 text-[#ff5600]">
+            BLURBEARS: a collection of 7777 BlurBears released on Ethereum @
+            0.0069 ETH per mint, 1 free per wallet.
+          </p>
+          <p className="text-lg font-semibold py-6 text-[#ff5600]">
+            Sweep and sell exclusively on the blur.io marketplace to be eligible
+            for the incoming $BLUR airdrops!
           </p>
 
           {isConnected ? (
@@ -50,14 +55,14 @@ function Hero({}: Props) {
               <div className="flex gap-4 items-center">
                 <button
                   onClick={() => setCount(Math.max(count - 1, 1))}
-                  className="rounded-xl bg-white px-4 py-2 font-bold text-xl"
+                  className="rounded-xl bg-white px-4 py-2 font-bold text-xl text-[#0a0504] bg-[#ff5600]"
                 >
                   -
                 </button>
-                <h1 className="text-3xl font-bold">{count}</h1>
+                <h1 className="text-3xl font-bold text-[#ff5600]">{count}</h1>
                 <button
                   onClick={() => setCount(Math.min(count + 1, 10))}
-                  className="rounded-xl bg-white px-4 py-2 font-bold text-xl"
+                  className="rounded-xl bg-white px-4 py-2 font-bold text-xl text-[#0a0504] bg-[#ff5600]"
                 >
                   +
                 </button>
@@ -66,7 +71,7 @@ function Hero({}: Props) {
                 onClick={() => {
                   mint?.();
                 }}
-                className="rounded-xl bg-white px-10 py-4 font-bold text-xl my-4 shadow-xl uppercase"
+                className="rounded-xl bg-white px-10 py-4 font-bold text-xl my-4 shadow-xl uppercase text-[#0a0504] bg-[#ff5600]"
                 disabled={isMintLoading}
                 data-mint-loading={isMintLoading}
                 data-mint-started={isMintStarted}
@@ -97,15 +102,20 @@ function Hero({}: Props) {
             </div>
           )}
 
-          <ConnectButton />
+          {/* <ConnectButton /> */}
+          <CustomButton />
           {isMinted && (
-            <div className="text-lg font-semibold py-6">
+            <div className="text-lg font-semibold py-6 text-[#ff5600]">
               Transaction successful! Thanks for minting a Blur Bear!
             </div>
           )}
         </div>
-        <div className="">
-          <img src="/bear.png" alt="" />
+        <div className="rounded-xl shadow-xl border-2 border-[#ff5600] p-6 basis-2/5">
+          <img
+            src="/blurbearnobg.png"
+            alt=""
+            className="rounded-xl shadow-xl"
+          />
         </div>
       </div>
     </div>
